c5fcc20ae0a8afed6dee107f6ef4052f088687ce
Ademola Emmanuel <141020350+codingrot17@users.noreply.github.com>
2023-09-24 20:34:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function toggleNightMode() {
     root.style.setProperty("--building-color2", "#000");
     root.style.setProperty("--building-color3", "#000");
     root.style.setProperty("--building-color4", "#000");
-    root.style.setProperty("--window-color1", "#777");
-    root.style.setProperty("--window-color2", "#777");
-    root.style.setProperty("--window-color3", "#777");
-    root.style.setProperty("--window-color4", "#777");
+    root.style.setProperty("--window-color1", "#538cc6");
+    root.style.setProperty("--window-color2", "#8cd9b3");
+    root.style.setProperty("--window-color3", "#d98cb3");
+    root.style.setProperty("--window-color4", "#8cb3d9");
     const sky = document.querySelector(".sky");
     sky.style.background =
       "radial-gradient(closest-corner circle at 15% 15%, #ccc, #ccc 20%, #445 21%, #223 100%)";
